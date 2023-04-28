46492597c454519164612cbeec0a510420c89097
lacaz3tte <pahomov.oleg.199777@gmail.com>
2023-04-28 18:55:45 +0300

    update styles

--- src/components/MainPanel/Input/Input.tsx
+++ src/components/MainPanel/Input/Input.tsx
@@ -6,7 +6,7 @@ const Input = () => {
     <div className="h-full flex flex-col justify-center">
       <div>
         <input
-          className="outline-none bg-transparent border-b-[1px] rounded-sm w-[450px] p-1 focus:bg-gray-100 font-Ubuntu"
+          className="outline-none bg-transparent border-b-[1px] rounded-full w-[450px] p-2 focus:bg-gray-100 font-Ubuntu px-5"
           placeholder="НАЙТИ"
           type="text" />
         <div className="mx-2 px-5 p-2 bg-[#4E6BAD] rounded-full text-white active:opacity-40 font-Ubuntu inline-flex items-center justify-center hover:cursor-pointer hover:bg-sky-600 transition-all">
